058fa93a73f5fbb9d1779c6bf9099ef498c86e90
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-10-19 21:17:56 +0530

    [Saravanan]: Added program to using rest controller

---
+++ src/main/java/com/bridgelabz/demo/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+	
+	@RequestMapping(value = {"","/","/home"})
+	public String sayHello() {
+		return "Hello from BridgeLabz";
+	}
+
+}
