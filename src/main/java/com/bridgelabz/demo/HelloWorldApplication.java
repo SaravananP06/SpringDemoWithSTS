17e635d45b5c0feae07e026d7744a49e019ffabc
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-10-19 20:50:28 +0530

    [Saravanan]: Added program to display message in webpage

--- src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
+++ src/main/java/com/bridgelabz/demo/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.bridgelabz.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Saravanan");	
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
